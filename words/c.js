3080c43ebda58ce2433b9b38621dbc5760575f8f
v_wxiahe <v_wxiahe@tencent.com>
2020-10-30 18:12:05 +0800

    update

--- words/c.js
+++ words/c.js
@@ -292,5 +292,201 @@ module.exports = {
         "audio": "US/command.mp3",
       }
     }
+  },
+  "compatible": {
+    "word": "compatible",
+    "interpretation": "adj. 可共存的；协调的；兼容的",
+    "pronunciation": {
+      "UK": {
+        "phonogram": "[kəmˈpæt.ə.bəl]",
+        "audio": "UK/compatible.mp3",
+      },
+      "US": {
+        "phonogram": "[kəmˈpæt̬.ə.bəl]",
+        "audio": "US/compatible.mp3",
+      }
+    }
+  },
+  "compile": {
+    "word": "compile",
+    "interpretation": "v. 汇编；编纂；编辑；编制",
+    "pronunciation": {
+      "UK": {
+        "phonogram": "[kəmˈpaɪl]",
+        "audio": "UK/compile.mp3",
+      },
+      "US": {
+        "phonogram": "[kəmˈpaɪl]",
+        "audio": "UK/compile.mp3",
+      }
+    }
+  },
+  "component": {
+    "word": "component",
+    "interpretation": "n. 组成部分；成分；零部件",
+    "pronunciation": {
+      "UK": {
+        "phonogram": "[kəmˈpəʊ.nənt]",
+        "audio": "UK/component.mp3",
+      },
+      "US": {
+        "phonogram": "[kəmˈpoʊ.nənt]",
+        "audio": "UK/component.mp3",
+      }
+    }
+  },
+  "composition": {
+    "word": "composition",
+    "interpretation": "n. 成分；构成；结构; （音乐）作品，乐曲; ",
+    "pronunciation": {
+      "UK": {
+        "phonogram": "[ˌkɒm.pəˈzɪʃ.ən]",
+        "audio": "UK/composition.mp3",
+      },
+      "US": {
+        "phonogram": "[ˌkɑːm.pəˈzɪʃ.ən]",
+        "audio": "UK/composition.mp3",
+      }
+    }
+  },
+  "concept": {
+    "word": "concept",
+    "interpretation": "n. 原则；概念；观念；想法",
+    "pronunciation": {
+      "UK": {
+        "phonogram": "[ˈkɒn.sept]",
+        "audio": "UK/concept.mp3",
+      },
+      "US": {
+        "phonogram": "[ˈkɑːn.sept]",
+        "audio": "US/concept.mp3",
+      }
+    }
+  },
+  "concrete": {
+    "word": "concrete",
+    "interpretation": "adj. 确定的，确实的；具体的，有形的，实在的; n. 混凝土",
+    "pronunciation": {
+      "UK": {
+        "phonogram": "[ˈkɒŋ.kriːt]",
+        "audio": "UK/concrete.mp3",
+      },
+      "US": {
+        "phonogram": "[ˈkɑːn.kriːt]",
+        "audio": "US/concrete.mp3",
+      }
+    }
+  },
+  "configuration": {
+    "word": "configuration",
+    "interpretation": "n. 布局；构造，结构；格局",
+    "pronunciation": {
+      "UK": {
+        "phonogram": "[kənˌfɪɡ.əˈreɪ.ʃən]",
+        "audio": "UK/configuration.mp3"
+      },
+      "US": {
+        "phonogram": "[kənˌfɪɡ.jəˈreɪ.ʃən]",
+        "audio": "UK/configuration.mp3"
+      }
+    }
+  },
+  "connection": {
+    "word": "connection",
+    "interpretation": "n. （事物间的）连接，联结；连接物; （网络,资料库） 连接; ",
+    "pronunciation": {
+      "UK": {
+        "phonogram": "[kəˈnek.ʃən]",
+        "audio": "UK/connection.mp3"
+      },
+      "US": {
+        "phonogram": "[kəˈnek.ʃən]",
+        "audio": "UK/connection.mp3"
+      }
+    }
+  },
+  "constraint": {
+    "word": "constraint",
+    "interpretation": "n. 限制；束缚；约束",
+    "pronunciation": {
+      "UK": {
+        "phonogram": "[kənˈstreɪnt]",
+        "audio": "UK/constraint.mp3"
+      },
+      "US": {
+        "phonogram": "[kənˈstreɪnt]",
+        "audio": "UK/constraint.mp3"
+      }
+    }
+  },
+  "construct": {
+    "word": "construct",
+    "interpretation": "v. 建造；构筑；构成，组成 ; n. 构想；观念；编造，杜撰",
+    "pronunciation": {
+      "UK": {
+        "phonogram": "[kənˈstrʌkt]",
+        "audio": "UK/construct.mp3"
+      },
+      "US": {
+        "phonogram": "[kənˈstrʌkt]",
+        "audio": "UK/construct.mp3"
+      }
+    }
+  },
+  "container": {
+    "word": "container",
+    "interpretation": "n. （尤指用于运输或储存的）容器",
+    "pronunciation": {
+      "UK": {
+        "phonogram": "[kənˈteɪ.nər]",
+        "audio": "UK/container.mp3"
+      },
+      "US": {
+        "phonogram": "[kənˈteɪ.nɚ]",
+        "audio": "US/container.mp3"
+      }
+    }
+  },
+  "context": {
+    "word": "context",
+    "interpretation": "n. （事情存在、发生的）背景，环境; 上下文；语境; ",
+    "pronunciation": {
+      "UK": {
+        "phonogram": "[ˈkɒn.tekst]",
+        "audio": "UK/context.mp3"
+      },
+      "US": {
+        "phonogram": "[ˈkɑːn.tekst]",
+        "audio": "US/context.mp3"
+      }
+    }
+  },
+  "const": {
+    "word": "const",
+    "interpretation": "常数（constant 的缩写，C++ 关键字）",
+    "pronunciation": {
+      "UK": {
+        "phonogram": "[]",
+        "audio": ""
+      },
+      "US": {
+        "phonogram": "[]",
+        "audio": ""
+      }
+    }
+  },
+  "constant": {
+    "word": "constant",
+    "interpretation": "adj. 经常发生的；连续发生的；连续不断的 ; 恒久不变的；稳定的; ",
+    "pronunciation": {
+      "UK": {
+        "phonogram": "[ˈkɒn.stənt]",
+        "audio": "UK/constant.mp3"
+      },
+      "US": {
+        "phonogram": "[ˈkɑːn.stənt]",
+        "audio": "US/constant.mp3"
+      }
+    }
   }
 }
